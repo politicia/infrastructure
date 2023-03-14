e1c7eac75607eaf4be3cf297df786755b0fe8040
Yany Choi <51287461+YanyChoi@users.noreply.github.com>
2023-03-14 13:13:45 +0900

    fix network module

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -14,7 +14,7 @@ resource "aws_internet_gateway" "politicia_internet_gw" {
   vpc_id = aws_vpc.politicia_vpc.id
 
   depends_on = [
-    aws_vpc.policitia_vpc
+    aws_vpc.politicia_vpc
   ]
 
   tags = {
@@ -23,12 +23,17 @@ resource "aws_internet_gateway" "politicia_internet_gw" {
   }
 }
 
-resource "aws_route_table" "route_table" {
+resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.politicia_vpc.id
+
+  depends_on = [
+    aws_vpc.politicia_vpc,
+    aws_internet_gateway.politicia_internet_gw
+  ]
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id = aws_internet_gateway.politicia_internet_gw.id
   }
 
   tags = {
@@ -37,7 +42,18 @@ resource "aws_route_table" "route_table" {
   }
 }
 
-resource "aws_subnet" "politicia_public_subnet" {
+resource "aws_route" "IG_route" {
+
+  depends_on = [
+    aws_route_table.public_route_table
+  ]
+
+  route_table_id         = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.politicia_internet_gw.id
+}
+
+resource "aws_subnet" "public_subnet" {
   vpc_id = aws_vpc.politicia_vpc.id
   count = length(var.public_subnets_cidr)
   cidr_block = element(var.public_subnets_cidr, count.index)
@@ -50,35 +66,54 @@ resource "aws_subnet" "politicia_public_subnet" {
   }
 }
 
-resource "aws_subnet" "private_subnet" {
-  vpc_id = aws_vpc.politicia_vpc.id
-  count = length(var.private_subnets_cidr)
-  cidr_block = element(var.private_subnets_cidr, count.index)
-  availability_zone = element(var.availability_zones, count.index)
-  map_public_ip_on_launch = false
+# resource "aws_subnet" "private_subnet" {
+#   vpc_id = aws_vpc.politicia_vpc.id
+#   count = length(var.private_subnets_cidr)
+#   cidr_block = element(var.private_subnets_cidr, count.index)
+#   availability_zone = element(var.availability_zones, count.index)
+#   map_public_ip_on_launch = false
 
-  tags = {
-    Name = "politicia-${var.environment}-${element(var.availability_zones, count.index)}-private-subnet"
-    Environment = var.environment
-  }
+#   tags = {
+#     Name = "politicia-${var.environment}-${element(var.availability_zones, count.index)}-private-subnet"
+#     Environment = var.environment
+#   }
+# }
+
+resource "aws_route_table_association" "public_subnet_association" {
+
+  depends_on = [
+    aws_subnet.public_subnet,
+    aws_route_table.public_route_table
+  ]
+  count = length(var.public_subnets_cidr)
+  subnet_id = element(aws_subnet.public_subnet.*.id, count.index)
+  route_table_id = aws_route_table.public_route_table.id
 }
 
-resource "aws_route_table_association" "subnet_1_association" {
-  subnet_id = aws_subnet.subnet_1.id
-  route_table_id = aws_route_table.route_table.id
+resource "aws_route_table_association" "associate_routetable_to_public_subnet" {
+
+  depends_on = [
+    aws_subnet.public_subnet,
+    aws_route_table.public_route_table
+  ]
+
+  count          = length(var.public_subnets_cidr)
+  subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
+  route_table_id = aws_route_table.public_route_table.id
 }
 
-resource "aws_route_table_association" "subnet_2_association" {
-  subnet_id = aws_subnet.subnet_2.id
-  route_table_id = aws_route_table.route_table.id
-}
+
+# resource "aws_route_table_association" "private_subnet_association" {
+#   subnet_id = aws_subnet.private_subnet.id
+#   route_table_id = aws_route_table.route_table.id
+# }
 
 resource "aws_security_group" "politicia_default_sg" {
   name        = "${var.environment}-default-sg"
   description = "Default SG to alllow traffic to/from the VPC"
-  vpc_id      = aws_vpc.policitia_vpc.id
+  vpc_id      = aws_vpc.politicia_vpc.id
   depends_on = [
-    aws_vpc.policitia_vpc
+    aws_vpc.politicia_vpc
   ]
 
   ingress {
--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,19 +1,24 @@
 variable "environment" {
     type = string
+    description = "Current environment between Dev/Prod"
 }
 
 variable "vpc_cidr" {
     type = string
+    description = "Subnet Mask of VPC"
 }
 
 variable "public_subnets_cidr" {
     type = list(string)
+    description = "Subnet Mask of Public Subnets as a List"
 }
 
 variable "private_subnets_cidr" {
     type = list(string)
+    description = "Subnet Mask of Private Subnets as a List"
 }
 
 variable "availability_zones" {
     type = list(string)
+    description = "List of Availability Zones in use"
 }
