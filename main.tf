30900c94bf835b1c5e032257a2274961c9100422
Yany Choi <51287461+YanyChoi@users.noreply.github.com>
2023-03-14 13:14:15 +0900

    change file structure

--- main.tf
+++ main.tf
@@ -1,9 +1,22 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-yany"
-    key    = "terraform/terraform.tfstate"
-    region         = "ap-northeast-2"  
-    encrypt        = true
-  }
+locals {
+  production_availability_zones  = ["${var.region}a", "${var.region}c", "${var.region}d"]
+  development_availability_zones = ["${var.region}a", "${var.region}c"]
+  environment = terraform.workspace
+  tag = "managed via Terraform"
 }
 
+module "network" {
+  source                   = "./modules/network"
+  environment              = local.environment
+  vpc_cidr                 = var.vpc_cidr
+  public_subnets_cidr      = var.public_subnets_cidr
+  private_subnets_cidr     = var.private_subnets_cidr
+  availability_zones       = local.environment == "development" ? local.development_availability_zones : local.production_availability_zones
+}
+
+module "s3" {
+  source      = "./modules/s3-bucket"
+  environment = local.environment
+  politicia_media_cors_allowed_origins = var.politicia_media_cors_allowed_origins
+  politicia_thumbnail_cors_allowed_origins = var.politicia_thumbnail_cors_allowed_origins
+}
--- providers.tf
+++ providers.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-yany"
+    key    = "terraform/terraform.tfstate"
+    region         = "ap-northeast-2"  
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   access_key = var.AWS_ACCESS_KEY
   secret_key = var.AWS_SECRET_KEY
